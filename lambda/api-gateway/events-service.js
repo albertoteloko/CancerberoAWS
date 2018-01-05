82a7c0d08d17949c6e3eafba1cc5e79ef7974a1c
Alberto Crespo <alberto.cresposanchez@zooplus.com>
2018-01-05 13:14:21 +0100

    Improve the pins list

--- lambda/api-gateway/events-service.js
+++ lambda/api-gateway/events-service.js
@@ -9,8 +9,8 @@ module.exports = {
             return handlePing(event);
         } else if (event.type === 'pin-activated') {
             return handlePinActivation(event);
-            } else if (event.type === 'pin-change') {
-                return handlePinChange(event);
+        } else if (event.type === 'pin-change') {
+            return handlePinChange(event);
             // } else if (event.name === 'status-change') {
             //     return handleNodeStatusChange(event);
         } else {
@@ -39,12 +39,10 @@ function handlePinActivation(event) {
     const nodeId = event.nodeId;
     return nodeRepository.read(nodeId).then(node => {
         if (node != null) {
-            let pins = node.pins.filter(p => p.id === event.pinId);
-            if (pins.length !== 0) {
-                for (let pin of pins) {
-                    setPinActivations(pin, event);
-                    setPinReading(pin, event);
-                }
+            let pin = node.modules.alarm.pins[event.pinId];
+            if (pin !== undefined) {
+                setPinActivations(pin, event);
+                setPinReading(pin, event);
                 return nodeRepository.save(node).then(result => {
                     return eventRepository.save(event);
                 });
@@ -61,14 +59,12 @@ function handlePinChange(event) {
     const nodeId = event.nodeId;
     return nodeRepository.read(nodeId).then(node => {
         if (node != null) {
-            let pins = node.pins.filter(p => p.id === event.pinId);
-            if (pins.length !== 0) {
-                for (let pin of pins) {
-                    if((pin.readings !== undefined) &&(pin.readings.value === event.value)){
-                        return Promise.reject("Node " + nodeId + " and pin " + event.pinId + " does not change its value");
-                    }
-                    setPinReading(pin, event);
+            let pin = node.modules.alarm.pins[event.pinId];
+            if (pin !== undefined) {
+                if ((pin.readings !== undefined) && (pin.readings.value === event.value)) {
+                    return Promise.reject("Node " + nodeId + " and pin " + event.pinId + " does not change its value");
                 }
+                setPinReading(pin, event);
                 return nodeRepository.save(node).then(result => {
                     return eventRepository.save(event);
                 });
@@ -82,7 +78,7 @@ function handlePinChange(event) {
 }
 
 function setPinActivations(pin, event) {
-    if(pin.activations === undefined){
+    if (pin.activations === undefined) {
         pin.activations = {};
     }
     pin.activations.timestamp = event.timestamp;
@@ -97,57 +93,27 @@ function setPinReading(pin, event) {
     pin.readings.value = event.value;
 }
 
+function handleAlarmStatusChange(event) {
+    const nodeId = event.nodeId;
+    const status = event.status;
+    return nodeRepository.getNode(nodeId).then(node => {
+        if (node != null) {
+            if (status !== node.status) {
+                return nodeStatusChangeEventRepository.storeStatusChange(node.id, status)
+            } else {
+                return Promise.reject("No changes in the node status");
+            }
+        } else {
+            return Promise.reject("Node " + nodeId + " not found")
+        }
+    });
+}
 
-// function handlePinChange(event) {
-//     const nodeId = event.nodeId;
-//     const pinId = event.pinId;
-//     const value = event.value;
-//     return Promise.all([
-//         nodeRepository.getNode(nodeId),
-//         pinChangeEventRepository.getLatestPinStatus(nodeId, pinId)
-//     ]).then(values => {
-//         const node = values[0];
-//         const latestStatus = values[1];
-//
-//         if (node != null) {
-//             const pin = findPin(node.pins, pinId);
-//
-//             if (pin != null) {
-//                 if (value !== latestStatus) {
-//                     return pinChangeEventRepository.storePinChange(node.id, pinId, value)
-//                 } else {
-//                     return Promise.reject("No changes in the pin value");
-//                 }
-//             } else {
-//                 return Promise.reject("Node " + nodeId + " does not have a pin " + pinId)
-//             }
-//         } else {
-//             return Promise.reject("Node " + nodeId + " not found")
-//         }
-//     });
-// }
-//
-// function handleNodeStatusChange(event) {
-//     const nodeId = event.nodeId;
-//     const status = event.status;
-//     return nodeRepository.getNode(nodeId).then(node => {
-//         if (node != null) {
-//             if (status !== node.status) {
-//                 return nodeStatusChangeEventRepository.storeStatusChange(node.id, status)
-//             } else {
-//                 return Promise.reject("No changes in the node status");
-//             }
-//         } else {
-//             return Promise.reject("Node " + nodeId + " not found")
-//         }
-//     });
-// }
-//
-// function findPin(pins, pinId) {
-//     for (let pin of pins) {
-//         if (pin.pin_id === pinId) {
-//             return pin;
-//         }
-//     }
-//     return null;
-// }
+function findPin(pins, pinId) {
+    for (let pin of pins) {
+        if (pin.pin_id === pinId) {
+            return pin;
+        }
+    }
+    return null;
+}
