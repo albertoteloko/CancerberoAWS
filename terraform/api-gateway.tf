402c997c96ee7c6ebbae14f68e5f173afe17d0b3
Alberto Crespo <albertocresposanchez@gmail.com>
2018-02-04 19:07:21 +0100

    Apply cognito credentials

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -151,11 +151,9 @@ resource "aws_api_gateway_method" "node-action" {
   rest_api_id   = "${aws_api_gateway_rest_api.domo-slave-api.id}"
   resource_id   = "${aws_api_gateway_resource.node-action.id}"
   http_method   = "POST"
-  authorization = "NONE"
-  api_key_required = true
-//  authorization = "COGNITO_USER_POOLS"
-//  authorizer_id = "${aws_cloudformation_stack.cancerbero_api_authorizer.outputs["id"]}"
-//  api_key_required = false
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = "${aws_cloudformation_stack.cancerbero_api_authorizer.outputs["id"]}"
+  api_key_required = false
 }
 
 resource "aws_api_gateway_method" "installations" {
